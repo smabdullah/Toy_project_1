b1ff1b14306d40891b6980d6957159e79eae4d4e
SM Abdullah <sma.csedu@gmail.com>
2019-03-18 22:29:08 +1100

    Decision tree visualisation using the graphviz

--- airfoil_decisionTree.py
+++ airfoil_decisionTree.py
@@ -9,6 +9,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+feature_name = [
+        'Frequency', 'Angle of attack', 'Chord length', 'Free-stream velocity', 'suction thickness']
+
+
 # importing the dataset
 dataset = pd.read_csv('airfoil_self_noise.dat', sep = '\t', header = None, names = [
         'Frequency', 'Angle of attack', 'Chord length', 'Free-stream velocity', 'suction thickness', 'sound pressure level'])
@@ -35,4 +39,12 @@ variance_score = explained_variance_score(y_test, y_pred)
 R2 = r2_score(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
 
+# Visualiasing the decision tree
+from sklearn import tree
+import graphviz
+
+dot_data = tree.export_graphviz(dt_regressor, out_file=None, feature_names = feature_name, class_names = 'sound pressure level',
+                                filled = True, rounded = True, special_characters = True)
+graph = graphviz.Source(dot_data)
+graph.render('decision-tree')
 
