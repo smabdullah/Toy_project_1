9d31d933949ac280d5ab2df6b5157faeb475ac35
SM Abdullah <sma.csedu@gmail.com>
2019-03-20 13:24:28 +1100

    Initial visualisation file with libraries imported

---
+++ data_Visualisation_Bokeh.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 13:21:10 2019
+
+@author: SM
+"""
+
+import pandas as pd
+import numpy as np
+from bokeh.plotting import figure, output_file, show
