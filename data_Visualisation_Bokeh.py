940d3570462934b01cbf1844189116bf15029b4d
SM Abdullah <sma.csedu@gmail.com>
2019-03-20 13:26:59 +1100

    read the dataset as dataframe and separate feature matrix and vector

--- data_Visualisation_Bokeh.py
+++ data_Visualisation_Bokeh.py
@@ -4,7 +4,15 @@ Created on Wed Mar 20 13:21:10 2019
 
 @author: SM
 """
-
+# Importing library
 import pandas as pd
 import numpy as np
 from bokeh.plotting import figure, output_file, show
+
+# Read data
+dataset = pd.read_csv('airfoil_self_noise.dat', sep = '\t', header = None, names = [
+        'Frequency', 'Angle of attack', 'Chord length', 'Free-stream velocity', 'suction thickness', 'sound pressure level'])
+# feature matrix
+X = dataset.iloc[:,:-1].values
+# target vector
+y = dataset.iloc[:,5].values
